9a15beb5103314e9da5badd9c514640dbae71de7
Evgenii Sopov <mrseakg@gmail.com>
2025-11-23 13:31:27 +0700

    renamed sibgamejam2025 -> TheGuidingThread. and fixed cmake with suffixes _linux / _windows

--- launch_editor.bat
+++ launch_editor.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 set APP="bin\Editor_x64.exe"
 if exist %APP% (
-	start "" %APP% -video_app auto -video_refresh 0 -video_debug 0 -main_window_size 1280 720 -main_window_resizable 1 -main_window_fullscreen 0 -render_vsync 0 -video_offscreen 0 -sound_app auto -data_path ../data/ -extern_plugin UnigineFbxImporter,UnigineGLTFImporter,UnigineFbxExporter,UnigineUsdExchanger -console_command "config_autosave 1 && world_load \"sibgamejam2025\""
+	start "" %APP% -video_app auto -video_refresh 0 -video_debug 0 -main_window_size 1280 720 -main_window_resizable 1 -main_window_fullscreen 0 -render_vsync 0 -video_offscreen 0 -sound_app auto -data_path ../data/ -extern_plugin UnigineFbxImporter,UnigineGLTFImporter,UnigineFbxExporter,UnigineUsdExchanger -console_command "config_autosave 1 && world_load \"TheGuidingThread\""
 ) else (
 	set MESSAGE=%APP% not found"
 	echo !MESSAGE!
